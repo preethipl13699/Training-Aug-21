01e6ad7832dbed84e874ae8f8183702d7e8bd305
preethipl13699 <preethipl13699@gmail.com>
2021-08-16 19:10:21 +0530

    REFS: #427 and fixes#427 REFS: #464 and fixes#464 REFS: #474 and fixes#474 REFS: #510 and fixes#510 SQL Day1-4 tasks

--- SQL/Preethi P.L/Day1/ALTER.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE jobhistory ADD CONSTRAINT fk_job_id FOREIGN KEY(JobId) REFERENCES jobs(JobId)  
-
-ALTER TABLE jobhistory DROP CONSTRAINT fk_job_id
-
-ALTER TABLE jobhistory ADD Location varchar(20)
-
--- SQL/Preethi P.L/Day1/SQLAssignment.sql
+++ SQL/Preethi P.L/Day1/SQLAssignment.sql
@@ -1,31 +1,47 @@
-CREATE TABLE car_employees
+CREATE TABLE employees
 (
-	Employee_id int PRIMARY KEY IDENTITY(100,1),
-	EmployeeName varchar(20) NOT NULL,
+	Employee_id int PRIMARY KEY AUTO_INCREMENT,
+	EmployeeName varchar(30) NOT NULL,
 	Email varchar(30) NOT NULL,
 	MobileNo numeric(10) NOT NULL,
 	EmployeeAddress varchar(200) NOT NULL,
 	Bdate date NOT NULL,
+    	HireDate date NOT NULL,
 	Salary int NOT NULL
 	
-)
+);
+
+CREATE TABLE customers
+(
+	Customer_id int PRIMARY KEY AUTO_INCREMENT,
+	CustomerName varchar(30) NOT NULL,
+	CustomerEmail varchar(30) NOT NULL,
+	CustomerMobileNo numeric(10) NOT NULL,
+	CustomerAddress varchar(200) NOT NULL
+);
 
 CREATE TABLE inventory
 (
-	CarId int PRIMARY KEY IDENTITY(1,1),
+	CarId int PRIMARY KEY AUTO_INCREMENT,
 	CarModel varchar(20) NOT NULL,
 	Price int NOT NULL
-)
+);
 
 CREATE TABLE sales
 (
-	SalesId int PRIMARY KEY IDENTITY(1,1),
-	Employee_id int FOREIGN KEY REFERENCES car_employees(Employee_id),
-	CarId int FOREIGN KEY REFERENCES inventory(CarId),
-)
+	SalesId int PRIMARY KEY AUTO_INCREMENT,
+	Employee_id int, 
+	CarId int,
+    	Customer_Id int,
+    	FOREIGN KEY(Employee_id) REFERENCES employees(Employee_id),
+    	FOREIGN KEY(CarId) REFERENCES inventory(CarId),
+    	FOREIGN KEY(Customer_Id) REFERENCES customers(Customer_Id)
+);
+
 CREATE TABLE commission
 (
-	CommissionId int PRIMARY KEY IDENTITY(1,1),
-	Employee_id int FOREIGN KEY REFERENCES car_employees(Employee_id),
-	Commission_Percentage varchar(4)
-)
+	CommissionId int PRIMARY KEY AUTO_INCREMENT,
+	Employee_id int, 
+	Commission int,
+    	FOREIGN KEY(Employee_id) REFERENCES employees(Employee_id)
+);
